1681c95aa4d92fd68ccaacec3be918f05d95e6f1
Athira K S <athiraks@Athiras-MacBook-Air.local>
2024-05-22 10:43:51 +0530

    Design fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import topImage from "./assets/images/bg-pattern-top.svg";
+import bottomImage from "./assets/images/bg-pattern-bottom.svg";
+import banner from "./assets/images/bg-pattern-card.svg";
+import avatar from "./assets/images/image-victor.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <img src={topImage} className="top-image" alt="top background"></img>
+      <img src={bottomImage} className='bottom-image' alt="bottom background"></img>
+      <div className="profile-card">
+        <img src={banner} className="banner" alt="banner"></img>
+        <div className="avatar">
+          <img src={avatar} className="banner" alt="avatar"></img>
+        </div>
+        <div className="details">
+          <div className="title">
+            Victor Crest <span className="age">26</span>
+          </div>
+          <div className="location">London</div>
+        </div>
+        <div className="media-details">
+          <div className="detail">
+            <div className="title">80K</div>
+            <div className="media-tag">Followers</div>
+          </div>
+          <div className="detail">
+            <div className="title">803K</div>
+            <div className="media-tag">Likes</div>
+          </div>
+          <div className="detail">
+            <div className="title">1.4K</div>
+            <div className="media-tag">Photos</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
